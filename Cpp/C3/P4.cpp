ac727f2695b3bae4b10c7a6b7caaed3faa248c0d
hukang <931801858@qq.com>
2017-03-03 19:52:49 +0800

    polished

---
+++ Cpp/C3/P4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char a,b;
+	int a1,b1;
+	a = getchar();
+	b = getchar();
+	putchar(a);
+	putchar(b);
+	a1 = (int)a;
+	b1 = (int)b; 
+	cout<<a1  <<b1 <<endl;
+	return 0; 
+}
---
+++ Cpp/README.md
@@ -0,0 +1,3 @@
+# Cpp
+Cpp learnning
+learn history
